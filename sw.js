cfab86f97c8d0d51d057a50511b071b4c1940814
na2hiro <na2hiro@users.noreply.github.com>
2019-12-28 13:08:11 +0000

    Automated deployment: Sat Dec 28 13:08:11 UTC 2019 d834da559dd6968eb35d02c1b97f827a944296db

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/dekadeka/precache-manifest.a943f5e6edadf2c313af62dad9fdf93f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
+importScripts("/dekadeka/precache-manifest.8437e99ae57ce0f5a01546d3290aa01b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
 
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
