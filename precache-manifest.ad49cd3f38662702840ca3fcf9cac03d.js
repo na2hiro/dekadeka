bae94dbac80b4838c44010985a5d925b2faae0dc
na2hiro <na2hiro@users.noreply.github.com>
2021-01-23 14:03:39 +0000

    Automated deployment: Sat Jan 23 14:03:39 UTC 2021 b30543efea5aed51a3beb7b1dfd97d7d5f8612fb

--- precache-manifest.ad49cd3f38662702840ca3fcf9cac03d.js
+++ precache-manifest.ad49cd3f38662702840ca3fcf9cac03d.js
@@ -6,7 +6,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dekadeka/assets/icon_512x512.5a99ac951e92f4c0e2886afcc4607390.png"
   },
   {
-    "revision": "6ba82177e21dc6ee8bee",
+    "revision": "6b13637a41784cb4d909",
     "url": "/dekadeka/dekadeka.js"
   },
   {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/dekadeka/precache-manifest.a8cc77205207a576264cbd3571628336.js", "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
+importScripts("/dekadeka/precache-manifest.ad49cd3f38662702840ca3fcf9cac03d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
 
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
